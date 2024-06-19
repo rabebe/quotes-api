b5674a7bba70709d9ca86e7faafa033aae2b442e
rabebe <ruthie.abebe@gmail.com>
2024-06-19 18:21:31 +0200

    Add v1 router and update entry point

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,10 @@
 const express = require("express"); 
+const v1Router = require("./v1/routes");
 
 const app = express(); 
 const PORT = process.env.PORT || 5500; 
 
-// For testing purposes 
-app.get("/", (req, res) => { 
-    res.send("<h2>Test</h2>"); 
-}); 
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
