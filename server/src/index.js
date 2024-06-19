7e7148e37b44c44fc07b773e75d0fa1c9e311767
rabebe <ruthie.abebe@gmail.com>
2024-06-19 18:32:52 +0200

    Delete index.js test file in src/v1/routes and hook up v1 quotes router

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
 const express = require("express"); 
-const v1Router = require("./v1/routes");
+const v1QuoteRouter = require("./v1/routes/quoteRoutes");
 
 const app = express(); 
 const PORT = process.env.PORT || 5500; 
 
-app.use("/api/v1", v1Router);
+app.use("/api/v1/quotes", v1QuoteRouter);
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
