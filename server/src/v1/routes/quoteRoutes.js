9e6e92759fecda875eded155616957f545012f58
rabebe <ruthie.abebe@gmail.com>
2024-06-19 23:15:41 +0200

    Fix all quotes connection

--- server/src/v1/routes/quoteRoutes.js
+++ server/src/v1/routes/quoteRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const { getAllQuotes, addQuote, getQuoteById, updateQuoteById, deleteQuoteById, getRandomQuote } = require("../../controllers/quoteController");
-const apicache = require("apicache");
 
 const router = express.Router();
-const cache = apicache.middleware;
 
-router.get("/", cache("2 minutes"), getAllQuotes);
+
+router.get("/all", getAllQuotes);
 
 router.get("/random", getRandomQuote);
 
