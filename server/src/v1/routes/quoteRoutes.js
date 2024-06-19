f00930159644ca60924691e3a75d801f896f8df5
rabebe <ruthie.abebe@gmail.com>
2024-06-19 18:29:28 +0200

    Define endpoints inside router

---
+++ server/src/v1/routes/quoteRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Get all quotes");
+});
+
+router.get("/random", (req, res) => {
+    res.send("Get a random quote");
+  });
+
+router.get("/:workoutId", (req, res) => {
+  res.send("Get quote by Id");
+});
+
+router.post("/", (req, res) => {
+  res.send("Create a new quote");
+});
+
+router.patch("/:quoteId", (req, res) => {
+  res.send("Update an existing quote");
+});
+
+router.delete("/:workoutId", (req, res) => {
+  res.send("Delete an existing quote");
+});
+
+module.exports = router;
