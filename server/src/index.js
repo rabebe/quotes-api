944b6f4b24d78562384974cf1c81b540934c7f6b
rabebe <ruthie.abebe@gmail.com>
2024-06-25 16:32:37 +0200

    Resolve CORS error

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,31 @@
 const express = require("express");
-const cors = require('cors');
 const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require('cors');
+const helmet = require("helmet");
 const apicache = require("apicache");
 const v1QuoteRouter = require("./v1/routes/quoteRoutes");
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express(); 
-const cache = apicache.middleware;
-const PORT = process.env.PORT || 5500; 
+const PORT = process.env.PORT || 5500;
 
+// Middleware
+app.use(helmet());
+
+
+// Configure CORS to allow requests from localhost:3000
 const corsOptions = {
-     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
 };
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
-app.use(cache("2 minutes"));
+app.use(apicache.middleware("2 minutes"));
+
+// Routes
 app.use("/api/v1/quotes", v1QuoteRouter);
-app.use(cors(corsOptions)); 
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`);
